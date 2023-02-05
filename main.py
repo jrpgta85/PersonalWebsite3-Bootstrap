fff4a724aac2b5bf5c778eb538a1013df2ef5d7c
Joe Pekol <jrpgta85@gmail.com>
2023-02-05 11:11:49 -0500

    The first commit for version control on Personal Website 3 with Bootstrap

---
+++ main.py
@@ -0,0 +1,13 @@
+# from flask import Flask, render_template
+#
+# app = Flask(__name__)
+#
+#
+# @app.route('/')
+# def home():
+#     return render_template("index.html")
+#
+#
+# if __name__ == '__main__':
+#     # app.run(host='localhost', port=5001, debug=True)
+#     app.run(debug=True, host='0.0.0.0') # view results on mobile phone
